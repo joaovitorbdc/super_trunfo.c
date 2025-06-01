bbd836528700c7f2b9d834f2630adeb79fe1acfa
João Vitor Souza <165968344+joaovitorbdc@users.noreply.github.com>
2025-06-01 21:03:25 +0000

    trunfo

---
+++ trunfo.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+    int main(){
+
+    char estado;
+    char codigo_carta;
+    char nome_cidade[50];
+    int populacao;
+    int pontos;
+    float area_km2;
+    float pib;
+
+        printf("O estado é: \n"),
+        scanf("%c", &estado),
+        printf("O estado é: %c\n" , estado),
+
+    }
